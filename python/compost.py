dd81603d962ea7dcfc9687228f77b000b7f4158a
jennamz48 <jennazukswert@gmail.com>
2016-11-21 19:22:20 -0500

    Compost feedstock test
    Plan for small program (just the steps, not any code)

---
+++ python/compost.py
@@ -0,0 +1,29 @@
+# Compost feedstock test - to determine if your compost recipe has an adequate C:N ratio
+
+#in this case, the compost recipe only has 2 ingredients
+
+#For the following, use raw_input to get the values and print relevant text beforehand to ask for input:
+#Prompt the user to input the %C ingredient 1
+#Prompt the user to input the %N ingredient 1
+#Prompt the user to input the weight of ingredient 1 (W1)
+
+#Prompt the user to input the %C ingredient 2 (C2, use raw_input)
+#Prompt the user to input the %N ingredient 2 (N2, use raw_input)
+#Prompt the user to input the weight of ingredient 2 (W2)
+
+#Calculate C:N ratio given weight and % for each ingredient
+    # multiply %C for ingredient 1 (C1) by weight (W1) to get carbon content (C1content)
+    # multiply %C for ingredient 2 (C2) by weight (W2) to get carbon content (C2content)
+    # multiply %N for ingredient 1 (N1) by weight (W1) to get nitrogen content (N1content)
+    # multiply %N for ingredient 2 (N2) by weight (W2) to get nitrogen content (N2content)
+    # Add C1content and C2content to get total C
+    # Add N1 content and N2 content to get total N
+    # Divide total_C by total_N
+    # save that final C:N value
+
+
+#If C:N < 20, print ("Your C:N ratio is too low. You need to add more C-rich material")
+
+#elif C:N > 30, print ("Your C:N ratio is too high. You need to add more N-rich material"
+
+#else (meaning it's between 20 and 30), print ("Your C:N ratio is OK!")
